a33807fc57c035bb1af68a6ec3531dfdbdfdaf20
jobizzness <jobizzness@gmail.com>
2018-10-10 20:16:50 +0800

    question template

--- src/components/discussion-editor.js
+++ src/components/discussion-editor.js
@@ -3,11 +3,27 @@ import '@polymer/iron-flex-layout/iron-flex-layout.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 import { IronOverlayBehavior } from '@polymer/iron-overlay-behavior/iron-overlay-behavior.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
+import { MDCSelect } from "@material/select";
+import { MDCTextField } from '@material/textfield'
+import { MDCFormField } from '@material/form-field'
+
+import SharedStyles from './shared-styles.html'
+import textField from './material/textfield.html'
+import formField from './material/form-field.html'
+import button from './material/button.html'
+import select from "./material/select.html";
+
 
 customElements.define('bn-discussion-editor', class extends mixinBehaviors(
     [IronOverlayBehavior], PolymerElement) {
     static get template() {
         return html`
+            ${html([
+                SharedStyles +
+                textField +
+                button +
+                formField + select
+            ])}
             <style>
             :host {
                 display: -webkit-box;
@@ -71,7 +87,7 @@ customElements.define('bn-discussion-editor', class extends mixinBehaviors(
                 -ms-flex-align: center;
                 -webkit-align-items: center;
                 align-items: center;
-                background-color: white;
+                background-color: var(--app-primary-color);
                 -webkit-box-shadow: 0 1px 2px 0 rgba(0,0,0,0.09);
                 box-shadow: 0 1px 2px 0 rgba(0,0,0,0.09);
                 padding-left: 2em;
@@ -83,11 +99,22 @@ customElements.define('bn-discussion-editor', class extends mixinBehaviors(
                 flex-shrink: 0;
             }
             .title{
-                font-size: 1.5rem;
-                color: var(--app-primary-color);
+                font-size: 1.3rem;
+                color: white;
+                font-weight: 400;
             }
             .flex{
                 flex: 1;
+            }
+            main{
+                padding: 2em;
+            }
+            form{
+                max-width: 700px;
+                margin: 0 auto;
+            }
+            form>*{
+                margin-bottom: 2em;
             }
             @media (max-width: 767px) {
                 :host {
@@ -110,6 +137,40 @@ customElements.define('bn-discussion-editor', class extends mixinBehaviors(
                     </button>
                 </header>
                 <main>
+                    <form>
+                        <section class="row">
+                            <div class="mdc-select flex">
+                                <select class="mdc-select__native-control">
+                                    <option value="" disabled selected></option>
+                                    <option value="grains">
+                                        Bread
+                                    </option>
+                                    <option value="vegetables">
+                                        Vegetables
+                                    </option>
+                                    <option value="fruit">
+                                        Fruit
+                                    </option>
+                                </select>
+                                <label class="mdc-floating-label">Select Category</label>
+                                <div class="mdc-line-ripple"></div>
+                            </div>
+                        </section>
+                        <section class="row">
+                            <div class="mdc-text-field mdc-text-field--textarea flex">
+                                <textarea id="textarea" class="mdc-text-field__input" rows="8" cols="40"></textarea>
+                                <label for="textarea" class="mdc-floating-label">Enter your question</label>
+                            </div>
+                        </section>
+
+                        <!-- Actions -->
+                        <section class="actions">
+                            <button 
+                                class="mdc-button mdc-button--raised" 
+                                on-click="register" 
+                                disabled$="[[loading]]">Submit</button>
+                        </section>
+                    </form>
                 </main>
             </div>
 `;
@@ -130,6 +191,14 @@ customElements.define('bn-discussion-editor', class extends mixinBehaviors(
         this.setAttribute('aria-modal', 'true');
         this.addEventListener('transitionend', (e) => this._transitionEnd(e));
         this.addEventListener('iron-overlay-canceled', (e) => this._onCancel(e));
+    }
+
+    connectedCallback(){
+        super.connectedCallback()
+
+        this.select = new MDCSelect(this.shadowRoot.querySelector('.mdc-select'))
+        this.shadowRoot.querySelectorAll('.mdc-text-field').forEach((node) => new MDCTextField(node));
+        this.shadowRoot.querySelectorAll('.mdc-form-field').forEach((node) => new MDCFormField(node));
     }
 
     _renderOpened() {
